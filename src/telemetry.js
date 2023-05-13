d32f695d496b21faf35f46d62f98e05c104c5880
David-Bellon <dadbc16@gmail.com>
2023-05-13 11:19:47 +0200

    final

--- src/telemetry.js
+++ src/telemetry.js
@@ -1,6 +1,3 @@
-
-
-
 function getTelemetry() {
     let img = document.getElementById("telemetry-img");
     let loader = document.getElementById("loader-id");
@@ -23,6 +20,43 @@ function getTelemetry() {
         race: Number(raceValue),
     }
     let url = "https://formula1-response.azurewebsites.net/Telemetry";
+    loader.style.display = "block";
+    fetch(url, {
+        method: "POST",
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data)
+    }).then(function (response) {
+        return response.blob();
+    }).then(function (myBlob) {
+        loader.style.display = "none";
+        var url = URL.createObjectURL(myBlob);
+        img.src = url;
+    });
+}
+
+function getPace() {
+    let img = document.getElementById("pace-img");
+    let loader = document.getElementById("loaderPace-id");
+
+    const driver1 = document.getElementById("driver1");
+    const driver1Value = driver1.value;
+
+    const driver2 = document.getElementById("driver2");
+    const driver2Value = driver2.value;
+
+    const driver3 = document.getElementById("driver3");
+    const driver3Value = driver3.value;
+
+    const race = document.getElementById("racePace-select");
+    const raceValue = race.value;
+
+    let data = {
+        driver1: driver1Value,
+        driver2: driver2Value,
+        driver3: driver3Value,
+        race: Number(raceValue),
+    }
+    let url = "https://formula1-response.azurewebsites.net/RacePace";
     loader.style.display = "block";
     fetch(url, {
         method: "POST",
